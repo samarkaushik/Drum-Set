71168baf6f2d66fe92b6dc906eab27ae55533fd1
samarkaushik <mayankkaushik52@gmail.com>
2020-08-12 13:55:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+// button presses
+for(var i = 0; i < 7; i++){
+document.querySelectorAll(".drum")[i].addEventListener("click" , handleClick);
+
+function handleClick(){
+    var buttonInnerHtml = this.innerHTML;
+    makeSound(buttonInnerHtml);
+    buttonAnimation(buttonInnerHtml);
+}};
+
+
+// key presses from keyboard
+document.addEventListener("keypress", function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+}
+);
+
+function makeSound(key){
+    switch (key) {
+        case "w": 
+            var w = new Audio("sounds/crash.mp3");
+            w.play();
+            break;
+        
+        case "a": 
+            var a = new Audio("sounds/kick-bass.mp3");
+            a.play();
+            break;
+
+        case "s": 
+            var s = new Audio("sounds/tom-1.mp3");
+            s.play();
+            break;
+
+        case "d": 
+            var d = new Audio("sounds/tom-2.mp3");
+            d.play();
+            break;
+        
+        case "j": 
+            var j = new Audio("sounds/tom-3.mp3");
+            j.play();
+            break;
+
+        case "k": 
+            var k = new Audio("sounds/tom-4.mp3");
+            k.play();
+            break;
+
+        case "l": 
+            var l = new Audio("sounds/snare.mp3");
+            l.play();
+            break;
+            
+        default:
+            console.log();
+            break;
+    }
+
+
+    }
+    function buttonAnimation(currentKey){
+        var activeButton = document.querySelector("."+ currentKey);
+        activeButton.classList.add("pressed");
+
+        setTimeout(function(){
+            activeButton.classList.remove("pressed");}, 100);
+
+}
